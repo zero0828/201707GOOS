e2bfc94b5e82b3484e1ae276cc12f4bf2d6d22eb
zero0828 <ching780828@hotmail.com>
2017-07-01 11:24:22 +0800

    add a  failed acceptance testing ( 測試空殼 )

---
+++ GOOS_SampleTests/pageObjects/BudgetCreateSteps.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using FluentAutomation;
+using TechTalk.SpecFlow;
+
+namespace GOOS_SampleTests.steps
+{
+    [Binding]
+    [Scope(Feature = "BudgetCreate")]
+    public class BudgetCreateSteps : FluentTest
+    {
+        private BudgetCreatePage _budgetCreatePage;
+
+
+
+        [BeforeScenario()]
+        public void BeforeScenario()
+        {
+            SeleniumWebDriver.Bootstrap(SeleniumWebDriver.Browser.Chrome);
+        }
+
+        public BudgetCreateSteps()
+        {
+            this._budgetCreatePage = new BudgetCreatePage(this);
+        }
+
+        [Given(@"go to adding budget page")]
+        public void GivenGoToAddingBudgetPage()
+        {
+            this._budgetCreatePage.Go();
+        }
+
+        [When(@"I add a buget (.*) for ""(.*)""")]
+        public void WhenIAddABugetFor(
+            int amount, string yearMonth)
+        {
+            this._budgetCreatePage
+                .Amount(amount)
+                .Month(yearMonth)
+                .AddBudget();
+        }
+
+        [Then(@"it should display ""(.*)""")]
+        public void ThenItShouldDisplay(string message)
+        {
+            this._budgetCreatePage.ShouldDisplay(message);
+        }
+    }
+
+
+}
---
+++ GOOS_SampleTests/steps/BudgetCreatePage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAutomation;
+
+namespace GOOS_SampleTests.steps
+{
+    public class BudgetCreatePage : PageObject<BudgetCreatePage>
+    {
+        public BudgetCreatePage(FluentTest test)
+            : base(test)
+        {
+        }
+
+        public BudgetCreatePage Amount(int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BudgetCreatePage Month(string yearMonth)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BudgetCreatePage AddBudget()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BudgetCreatePage ShouldDisplay(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
