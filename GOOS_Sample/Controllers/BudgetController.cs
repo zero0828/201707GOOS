0772fc772ffe1fde38f75a328fe8921e84f79c0e
zero0828 <ching780828@hotmail.com>
2017-07-01 11:58:21 +0800

    * 第一個綠燈 Feature: BudgetCreate Scenario: Add a budget successfully 已完成第一個驗收測試，執行AutoTest 得到 added successfully

---
+++ GOOS_Sample/Controllers/BudgetController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GOOS_Sample.Models.ViewModels;
+
+namespace GOOS_Sample.Controllers
+{
+    public class BudgetController : Controller
+    {
+
+        // GET: Budget
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(BudgetAddViewModel model)
+        {
+            ViewBag.Message = "added successfully";
+            return View(model);
+        }
+
+    }
+}
---
+++ GOOS_Sample/Models/ViewModels/BudgetAddViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GOOS_Sample.Models.ViewModels
+{
+    public class BudgetAddViewModel
+    {
+        public int Amount { get; set; }
+        public string Month { get; set; }
+    }
+}
