415606d05b999611a599abea7ab77452b34542a7
zero0828 <ching780828@hotmail.com>
2017-07-01 11:40:29 +0800

    第一個紅燈 Feature: BudgetCreate Scenario: Add a budget successfully 已完成第一個驗收測試，手動測試，如預期般得到 404 error，test failed

---
+++ GOOS_SampleTests/Controllers/PageContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOOS_SampleTests.Controllers
+{
+
+    internal class PageContext
+    {
+        public static string Domain
+        {
+            get { return "http://localhost:58527"; }
+        }
+    }
+}
--- GOOS_SampleTests/steps/BudgetCreatePage.cs
+++ GOOS_SampleTests/steps/BudgetCreatePage.cs
@@ -3,34 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using FluentAutomation;
+using GOOS_SampleTests.Controllers;
 
 namespace GOOS_SampleTests.steps
 {
     public class BudgetCreatePage : PageObject<BudgetCreatePage>
     {
-        public BudgetCreatePage(FluentTest test)
-            : base(test)
+        public BudgetCreatePage(FluentTest test): base(test)
         {
+            //this.Url = $"{PageContext.Domain}/budget/add";
+            this.Url = PageContext.Domain +"/budget/add";
         }
 
         public BudgetCreatePage Amount(int amount)
-        {
-            throw new NotImplementedException();
+        { 
+            I.Enter(amount.ToString()).In("#amount");
+            return this;
         }
 
         public BudgetCreatePage Month(string yearMonth)
         {
-            throw new NotImplementedException();
+            I.Enter(yearMonth).In("#month");
+            return this;
         }
 
         public BudgetCreatePage AddBudget()
         {
-            throw new NotImplementedException();
+            I.Click("input[type=\"submit\"]");
+            return this;
         }
 
         public BudgetCreatePage ShouldDisplay(string message)
         {
-            throw new NotImplementedException();
+            I.Assert.Text(message).In("#message");
+            return this;
         }
     }
 }
