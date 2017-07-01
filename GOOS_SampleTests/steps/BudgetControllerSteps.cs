67201235625019be891717d5b7663b4b53f4a867
zero0828 <ching780828@hotmail.com>
2017-07-01 14:27:42 +0800

    避免測試資料只能跑一次， 將每次資料庫還原 透過 EF 在每一次測試執行前，清除 table 的測試資料 -在 BudgetControllerSteps 的 `[BeforeScenario]` 清除資料

--- GOOS_SampleTests/steps/BudgetControllerSteps.cs
+++ GOOS_SampleTests/steps/BudgetControllerSteps.cs
@@ -19,7 +19,13 @@ namespace GOOS_SampleTests.steps
         [BeforeScenario()]
         public void BeforeScenario()
         {
+
             this._budgetController = new BudgetController();
+
+            using (var dbcontext = new NORTHWNDEntitiesforTest())
+            {
+                dbcontext.Database.ExecuteSqlCommand("TRUNCATE TABLE [Budgets]");
+            }
         }
 
         [When(@"add a budget")]
